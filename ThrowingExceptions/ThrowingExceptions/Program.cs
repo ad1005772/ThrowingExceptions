2bc7bc024a175fc6d14bd0fde308aa35d18ffda6
ad1005772 <ad1005772@otc.edu>
2025-02-13 10:07:55 -0600

    Step 3
    Trying to get the input to be usable over multiple methods

--- ThrowingExceptions/ThrowingExceptions/Program.cs
+++ ThrowingExceptions/ThrowingExceptions/Program.cs
@@ -2,20 +2,43 @@
 
 class Program
 {
+    public string userInput;
+    public string passInput;
     public static void Main(string[] args)
     {
-        string userInput;
-        string passInput;
-        string username = "adeross";
-        string password = "778899I";
         
+
         Console.WriteLine("Enter your username:");
         userInput = Console.ReadLine();
         Console.WriteLine("Enter your password:");
         passInput = Console.ReadLine();
-
+        
+       
         
     }
 
+    public void TestUsername()
+    {
+        string username = "cat";
+        if (userInput != username)
+        {
+            throw new InvalidUsernameException("Invalid Username");
+        }
+        
+}
+    public class InvalidUsernameException : System.Exception
+    {
+        public InvalidUsernameException(string message) : base(message)
+        {
+            
+        }
+    }
 
+    public class InvalidPasswordException : System.Exception
+    {
+        public InvalidPasswordException(string message) : base(message)
+        {
+            
+        }
+    }
 }
