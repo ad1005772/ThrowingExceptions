7748c17c2fd25822ebcf330d289ceceecfaf341d
ad1005772 <ad1005772@otc.edu>
2025-02-13 09:38:35 -0600

    Lab Setup
    Completed Lab Setup moving onto instructions

---
+++ ThrowingExceptions/ThrowingExceptions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string userInput;
+        string passInput;
+        string username = "adeross";
+        string password = "778899I";
+        
+        Console.WriteLine("Enter your username:");
+        userInput = Console.ReadLine();
+        Console.WriteLine("Enter your password:");
+        passInput = Console.ReadLine();
+
+        
+    }
+
+
+}
